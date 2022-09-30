4fc3fad1825362c0c4c15b118f62f03f8a522daa
Haja Mohideen <97336864+haja1221@users.noreply.github.com>
2022-09-30 07:59:12 +0530

    Add files via upload
    It is a program with method overloading concept

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+class Calculator{
+   
+  int add(int a,int b){
+    
+      return a+b;
+  }
+  int add(int a,int b,int c){
+      
+     return a+b+c;
+  }
+  double multiply(double a,double b){
+      
+     return a*b;
+  } 
+  double division(double a,double b){
+       
+     return a/b;
+  }
+  int sub(int a,int b){
+     
+      return a-b;
+  }
+
+
+}
+
---
+++ Store.java
@@ -0,0 +1,11 @@
+class Store{
+    public static void main(String[]args){
+     Calculator h=new Calculator();
+     System.out.println("The addition of two numbers are :"+h.add(10,20));
+     System.out.println("The addition of three numbers are :"+h.add(10,40,50));
+     System.out.println("The multiply of two numbers are :"+h.multiply(5,9));
+     System.out.println("The multiply of two numbers are :"+h.division(100,20));
+     System.out.println("The subration of two numbers are :"+h.sub(70,100));
+    }
+
+}
